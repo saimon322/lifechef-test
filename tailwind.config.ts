d5c66de3702739353524373954c9250d035835fd
saimon322 <saimon.kat23@gmail.com>
2023-11-05 22:33:26 +0300

    tailwind settings and extended properties

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,33 @@
 import type { Config } from 'tailwindcss'
+const colors = require('tailwindcss/colors')
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx,css}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        base: '#252728',
+        primary: '#34A34F',
+        'neutrals': {
+          300: '#E1E3E5',
+          400: '#BEC1C4',
+          500: '#87898C',
+        }
       },
+      spacing: {
+        '18': '4.5rem'
+      },
+      borderWidth: {
+        '1': '1px',
+        '1.5': '1.5px'
+      },
+      boxShadow: {
+        modal: '0px 4px 4px 0px rgba(0, 0, 0, 0.02), 0px 4px 12px 0px rgba(0, 0, 0, 0.04), 0px 8px 24px 0px rgba(0, 0, 0, 0.06), 0px 16px 32px 0px rgba(0, 0, 0, 0.08)'
+      }
     },
   },
   plugins: [],
